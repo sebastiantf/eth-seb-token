04eec79d18a22cf627d9926596d334a386c29b40
sebastiantf <sebastian.tharakan97@gmail.com>
2019-07-27 15:21:44 +0530

    name, symbol

--- contracts/SebToken.sol
+++ contracts/SebToken.sol
@@ -1,6 +1,8 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract SebToken {
+    string public name = 'Seb Token';
+    string public symbol = 'SEB';
     uint256 public totalSupply;
 
     mapping(address => uint256) public balanceOf;
--- test/SebToken.test.js
+++ test/SebToken.test.js
@@ -1,6 +1,21 @@
 const SebToken = artifacts.require("./SebToken.sol");
 
 contract("SebToken", function(accounts) {
+  it("initializes name and symbol", function() {
+    return SebToken.deployed()
+      .then(function(i) {
+        tokenInstance = i;
+        return tokenInstance.name();
+      })
+      .then(function(name) {
+        assert.equal(name, "Seb Token", "does not have correct name");
+        return tokenInstance.symbol();
+      })
+      .then(function(symbol) {
+        assert.equal(symbol, "SEB", "does not have correct symbol");
+      });
+  });
+
   it("sets totalSupply on deployment", function() {
     return SebToken.deployed()
       .then(function(i) {
@@ -8,19 +23,11 @@ contract("SebToken", function(accounts) {
         return tokenInstance.totalSupply();
       })
       .then(function(totalSupply) {
-        assert.equal(
-          totalSupply.toNumber(),
-          1000000,
-          "doesn't set totalSupply to 1000000"
-        );
+        assert.equal(totalSupply.toNumber(), 1000000, "doesn't set totalSupply to 1000000");
         return tokenInstance.balanceOf(accounts[0]);
       })
       .then(function(adminBalance) {
-        assert(
-          adminBalance.toNumber(),
-          1000000,
-          "Admin account balance is not 1000000"
-        );
+        assert(adminBalance.toNumber(), 1000000, "Admin account balance is not 1000000");
       });
   });
 });
