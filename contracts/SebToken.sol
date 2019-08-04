23ffd18764254a6a0a774a386842473dc5e0e2d5
sebastiantf <sebastian.tharakan97@gmail.com>
2019-08-04 10:30:12 +0530

    SebToken.sol comments

--- contracts/SebToken.sol
+++ contracts/SebToken.sol
@@ -1,13 +1,13 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract SebToken {
-    string public name = 'Seb Token';
-    string public symbol = 'SEB';
-    uint256 public totalSupply;
+    string public name = 'Seb Token';   // optional
+    string public symbol = 'SEB';       // optional
+    uint256 public totalSupply;         // required
 
-    mapping(address => uint256) public balanceOf;
+    mapping(address => uint256) public balanceOf; // required
 
-    event Transfer(
+    event Transfer(     // required
         address indexed _from,
         address indexed _to,
         uint256 _value
@@ -18,12 +18,12 @@ contract SebToken {
         totalSupply = _initialSupply;
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool success) {
+    function transfer(address _to, uint256 _value) public returns (bool success) {     // required
         require(balanceOf[msg.sender] >= _value, '');
 
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
 
-        emit Transfer(msg.sender, _to, _value);
+        emit Transfer(msg.sender, _to, _value);     // required
     }
 }
