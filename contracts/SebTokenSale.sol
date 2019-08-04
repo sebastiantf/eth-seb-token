6a9bbe999df4fa1c2580b8cf8e5868d3f64f82db
sebastiantf <sebastian.tharakan97@gmail.com>
2019-08-04 19:05:22 +0530

    init buyTokens(), tokensSold

--- contracts/SebTokenSale.sol
+++ contracts/SebTokenSale.sol
@@ -6,6 +6,7 @@ contract SebTokenSale {
     address admin;
     SebToken public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokensSold;
 
     constructor(SebToken _tokenContract, uint256 _tokenPrice) public {
         // Assign Admin
@@ -16,5 +17,16 @@ contract SebTokenSale {
 
         // Token Price
         tokenPrice = _tokenPrice;
+    }
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        // Require value = tokens
+        // Require contract has enough tokens
+        // Require that transfer is successful
+
+        // Track tokensSold
+        tokensSold += _numberOfTokens;
+
+        // Sell Event
     }
 }
--- test/SebTokenSale.test.js
+++ test/SebTokenSale.test.js
@@ -2,6 +2,7 @@ const SebTokenSale = artifacts.require("./SebTokenSale.sol");
 
 contract("SebTokenSale", function(accounts) {
   var tokenPrice = 1000000000000000; // Token price in wei = 0.001 ETH
+  var numberOfTokens;
   it("deploys and initializes correctly", function() {
     return SebTokenSale.deployed()
       .then(function(i) {
@@ -18,6 +19,23 @@ contract("SebTokenSale", function(accounts) {
       })
       .then(function(price) {
         assert.equal(price, tokenPrice, "token price is not set");
+      });
+  });
+
+  it("facilitates buying tokens", function() {
+    return SebTokenSale.deployed()
+      .then(function(i) {
+        tokenSaleInstance = i;
+        var buyer = accounts[1];
+        numberOfTokens = 10;
+        var value = numberOfTokens * tokenPrice;
+        return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: value });
+      })
+      .then(function(receipt) {
+        return tokenSaleInstance.tokensSold();
+      })
+      .then(function(sold) {
+        assert.equal(sold.toNumber(), numberOfTokens, "tokensSold not equal to numberOfTokens bought");
       });
   });
 });
