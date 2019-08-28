c79bb870d741722d6e9b0d0f8c7adbf94ef98bca
sebastiantf <sebastian.tharakan97@gmail.com>
2019-08-28 23:08:26 +0530

    change totalSupply test to async/await

--- test/SebToken.test.js
+++ test/SebToken.test.js
@@ -26,8 +26,14 @@ contract("SebToken", function(accounts) {
       }); */
   });
 
-  it("sets totalSupply on deployment", function() {
-    return SebToken.deployed()
+  it("sets totalSupply on deployment", async () => {
+    const totalSupply = await this.tokenInstance.totalSupply();
+    assert.equal(totalSupply.toNumber(), 1000000, "doesn't set totalSupply to 1000000");
+
+    const adminBalance = await tokenInstance.balanceOf(accounts[0]);
+    assert(adminBalance.toNumber(), 1000000, "Admin account balance is not 1000000");
+
+    /* return SebToken.deployed()
       .then(function(i) {
         tokenInstance = i;
         return tokenInstance.totalSupply();
@@ -38,7 +44,7 @@ contract("SebToken", function(accounts) {
       })
       .then(function(adminBalance) {
         assert(adminBalance.toNumber(), 1000000, "Admin account balance is not 1000000");
-      });
+      }); */
   });
 
   it("transfers ownership of tokens", function() {
